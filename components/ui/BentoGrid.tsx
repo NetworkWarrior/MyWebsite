a0260a5084a6db04c511a837d78332336063b1be
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2025-03-21 00:26:06 +0100

    configuration

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { BackgroundGradientAnimation } from "./GradientBg";
 
 export const BentoGrid = ({
   className,
@@ -59,18 +60,40 @@ export const BentoGridItem = ({
             <img
               src={img}
               alt={img}
-              className={cn(imgClassName, 'object-cover, object-cover')}
+              className={cn(imgClassName, 'object-cover, object-center')}
             />
           )}
+        </div>
+
+        <div className={`absolute right-0 -bottom-5 ${id===5 && 'w-full opacity-80'}`}>
+          {spareImg && (
+            <img 
+            src={spareImg}
+            alt={spareImg}
+            className={cn(imgClassName, 'object-cover, object-center w-full h-full')}
+            />
+          )}
+        </div>
+
+        {id===6 && (
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
+          </BackgroundGradientAnimation>
+        )}
+        <div className={cn(
+          titleClassName, "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+        )}>
+
+        <div className="font-sans text-sm font-extralight text-[#c1c2d3] md:text-xs lg:text-base z-10">
+          {description}
+        </div>
+
         </div>
       </div>
 
       <div className="transition duration-200 group-hover/bento:translate-x-2">
         <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
           {title}
-        </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-          {description}
         </div>
       </div>
     </div>
